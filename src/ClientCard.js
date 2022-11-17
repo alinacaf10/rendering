fd79dc28fbe8946e3950f4c4dba4cb64fd862bfe
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-11-17 22:27:15 +0400

    update

--- src/ClientCard.js
+++ src/ClientCard.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+let change=true
 export class ClientCard extends React.Component {
   render() {
     const { name, phone, card } = this.props;
     return (
       <div className="card" style={{ width: "18rem" }}>
         <div className="card-body">
-          <h5 className="card-title">Имя клиента: {name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Телефон: {phone}</h6>
-          <p className="card-text">Номер карты: {card}</p>
+          <h5 className="card-title">Имя клиента: {name||"Anonimous "}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Телефон: {change&&phone}</h6>
+          <p className="card-text">Номер карты: {card||"Card is not specified "}</p>
         </div>
       </div>
     );
